a8b1834f208819a95be1fe0f5b9efeb79b5dde95
ErnTKE <ernesth332@gmail.com>
2025-09-25 23:56:41 +0000

    Square Class complete (Ernest Harris)

--- HW3/Square.cpp
+++ HW3/Square.cpp
@@ -1,2 +1,14 @@
-#include "Square.h"                                     // implementation files (.cpp) must include the related header file
-                                                    // implementation files (.cpp) define class member and non-member functions
+#include "Square.h"   
+
+int main() {
+Square sq1; // Default constructor
+Square sq2(5); // Parameterized constructor
+
+print(sq1); // Should print Area: 1
+print(sq2); // Should print Area: 25
+
+sq1.setSide(4);
+print(sq1); // Should print Area: 16
+
+return 0;
+}
--- HW3/Square.h
+++ HW3/Square.h
@@ -1,1 +1,20 @@
 #include <iostream>                 // header files (.h) should include any and all libaries required for the included code to work
+
+#ifndef SQUARE_H
+#define SQUARE_H
+
+class Square {
+private:
+double side;
+public:
+Square(); 
+Square(double s); 
+double getSide() const; 
+void setSide(double s); 
+double area() const; 
+};
+
+
+void print(const Square& sq);
+
+#endif
